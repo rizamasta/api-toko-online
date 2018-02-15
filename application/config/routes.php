784a4a010e5dd9664fdd96b61485535196daf999
Riza Masta Sputra <rizamasta@gmail.com>
2018-02-15 18:36:42 +0700

    Add routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,11 +4,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route = array(
     'default_controller' => "Homepage/home/index",
+    /**
+     * authentication start
+     */
     'user/login' => "User/Login/index",
-    'user/test' => "User/User/test",
-    'user/logout' => "User/User/logout",
-    'user/login/auth' => "User/Login/auth",
-    'user/login/auth-social' => "User/Login/loginSosial",
+    'user/login/out' => "User/Login/out",
+    'user/login/auth' => "User/Login/authentication",
+    'user/login/auth-social' => "User/Login/social",
+    /**
+     * authentication end
+     */
+
+     /**
+      * usermanagement start
+      */
+      'user/list' => "User/User/index",
+      /**
+      * usermanagement end
+      */
     '404_override' => 'Custom/error_404',
     'translate_uri_dashes' => TRUE
 );
