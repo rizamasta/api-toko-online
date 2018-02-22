d26a99c06aa94443f0e9fb4115035e779ead0aee
Joshua Gerald <joshua.hutasoit@ikonsultan.com>
2018-02-22 17:09:28 +0700

    ~~

---
+++ application/logs/log-2018-02-22.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-02-22 01:45:22 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) D:\xampp\htdocs\4hire\system\database\drivers\mysqli\mysqli_driver.php 202
+ERROR - 2018-02-22 01:45:22 --> Unable to connect to the database
+ERROR - 2018-02-22 01:45:30 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) D:\xampp\htdocs\4hire\system\database\drivers\mysqli\mysqli_driver.php 202
+ERROR - 2018-02-22 01:45:30 --> Unable to connect to the database
+ERROR - 2018-02-22 01:45:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) D:\xampp\htdocs\4hire\system\database\drivers\mysqli\mysqli_driver.php 202
+ERROR - 2018-02-22 01:45:33 --> Unable to connect to the database
