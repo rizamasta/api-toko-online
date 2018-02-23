678c699ba922fea6a865673974feef026494d561
Joshua Gerald <joshua.hutasoit@ikonsultan.com>
2018-02-23 17:09:58 +0700

    question_category

--- application/modules/Quiz/controllers/Testcase.php
+++ application/modules/Quiz/controllers/Testcase.php
@@ -50,7 +50,7 @@ class Testcase extends Abstract_Controller
 
     public function generate(){
         $id =random_string('alnum', 8);
-        $qs= $this->getModelQuiz()->getQuestion(4);
+        $qs= $this->getModelQuiz()->getQuestion(1);
         $url ="";
         if(!empty($qs)){
             $generated_question = array();
