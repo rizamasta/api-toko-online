f1274c9078f2e73786ff030ac204de585b1b0e74
Joshua Gerald <joshua.hutasoit@ikonsultan.com>
2018-02-23 10:08:41 +0700

    just pull

--- application/logs/log-2018-02-22.php
+++ application/logs/log-2018-02-22.php
@@ -6,3 +6,9 @@ ERROR - 2018-02-22 01:45:30 --> Severity: Warning --> mysqli::real_connect(): (H
 ERROR - 2018-02-22 01:45:30 --> Unable to connect to the database
 ERROR - 2018-02-22 01:45:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) D:\xampp\htdocs\4hire\system\database\drivers\mysqli\mysqli_driver.php 202
 ERROR - 2018-02-22 01:45:33 --> Unable to connect to the database
+ERROR - 2018-02-22 02:41:18 --> Severity: Warning --> mkdir(): Invalid path D:\xampp\htdocs\4hire\system\libraries\Session\drivers\Session_files_driver.php 118
+ERROR - 2018-02-22 02:41:18 --> Severity: Error --> session_start(): Failed to initialize storage module: user (path: ) D:\xampp\htdocs\4hire\system\libraries\Session\Session.php 141
+ERROR - 2018-02-22 02:41:33 --> Severity: Warning --> mkdir(): Invalid path D:\xampp\htdocs\4hire\system\libraries\Session\drivers\Session_files_driver.php 118
+ERROR - 2018-02-22 02:41:33 --> Severity: Error --> session_start(): Failed to initialize storage module: user (path: ) D:\xampp\htdocs\4hire\system\libraries\Session\Session.php 141
+ERROR - 2018-02-22 02:45:25 --> Severity: error --> Exception: Session: Configured driver 'cookies' was not found. Aborting. D:\xampp\htdocs\4hire\system\libraries\Session\Session.php 231
+ERROR - 2018-02-22 02:45:32 --> Severity: error --> Exception: Session: Configured driver 'cookie' was not found. Aborting. D:\xampp\htdocs\4hire\system\libraries\Session\Session.php 231
