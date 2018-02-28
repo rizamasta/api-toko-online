6bf8eb6fae2d539bef74c1d7b28cdaa501ef9017
tami <ahmad.utami@ikonsultan.com>
2018-02-28 16:10:57 +0700

    fixing css

--- public_assets/includes/menu-landing.php
+++ public_assets/includes/menu-landing.php
@@ -8,9 +8,6 @@
             <li class="js_nav-item nav-item"><a class="nav-item-child nav-item-hover" href="<?php echo site_url('user/login/out')?>">Log Out</a></li>
             <?php else: ?>
             <li class="js_nav-item nav-item"><a class="nav-item-child nav-item-hover" href="<?php echo site_url('user/login')?>">Sign Up</a></li>
-            <!-- <li class="js_nav-item nav-item"><a class="nav-item-child nav-item-hover" href="#pricing">Log In</a></li> -->
-            <!-- <li class="js_nav-item nav-item"><a class="nav-item-child nav-item-hover" href="#work">Work</a></li> -->
-            <!-- <li class="js_nav-item nav-item"><a class="nav-item-child nav-item-hover" href="#service">Service</a></li> -->
             <li class="js_nav-item nav-item"><a class="nav-item-child nav-item-hover" href="<?php echo site_url('user/login')?>">Log In</a></li>
             <?php endif ?>
         </ul>
